77f53deff0badd412e6a4f67ff38f4d90fff94ec
jamesmanone <boostedx@me.com>
2017-08-13 00:40:15 -0400

    Polishing off

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports.DB = require('./lib/DB');
+module.exports.Model = require('./lib/Model');
+module.exports.ObjectId = require('./lib/ObjectId');
+module.exports.Schema = require('./lib/Schema');
